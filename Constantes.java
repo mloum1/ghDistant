165b84a70f609a8419cacdfc1fa796995a425837
mloum1 <maniloum4@gmail.com>
2025-01-28 21:54:20 +0100

    C10: ajout d'une nouvelle constante nom utilisateur

--- Constantes.java
+++ Constantes.java
@@ -1,4 +1,5 @@
 public class Constantes {
     public static final String HELLO_WORLD = "HELLO WORLD";
     public static final String TITLE = "Informations utilisateur";
+    public static final String SubTitle = "Votre nom";
 }
